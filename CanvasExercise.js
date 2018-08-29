bf38591d0eed3ff08c449065587abfd0404fea75
Yulio Aleman Jimenez <yulioaj290@github.com>
2018-08-29 13:30:52 -0400

    [FEATURE] Added code documentation

--- CanvasExercise.js
+++ CanvasExercise.js
@@ -5,17 +5,37 @@
  * Created by WebStorm
  */
 
+// Type of character for order exercises
 const ORDER_TYPE_ALPHA = 0,
     ORDER_TYPE_NUM = 1;
-
+/**
+ * Superclass CanvasExercise define general properties and initialization functions for all kind of canvas exercises.
+ */
 class CanvasExercise {
 
-    constructor(canvasElementId, canvasWidth = '', imageSrc = '', 
+    /**
+     * Constructor of CanvasExercise class.
+     * @param canvasElementId
+     * @param canvasWidth
+     * @param imageSrc
+     * @param lineWidth
+     * @param lineColor
+     * @param lineHead
+     * @param fontFamily
+     * @param fontSize
+     * @param fontColor
+     * @param fontStyle
+     * @param fontAlign
+     * @param fontBaseline
+     * @param mark
+     * @param orderType
+     */
+    constructor(canvasElementId, canvasWidth = '', imageSrc = '',
                 lineWidth = '2', lineColor = '#000000', lineHead = false, fontFamily = 'Arial',
                 fontSize = '20', fontColor = '#000000', fontStyle = 'regular', fontAlign = 'center',
                 fontBaseline = 'middle', mark = 'X', orderType = ORDER_TYPE_NUM) {
 
-        this._canvasElementId = canvasElementId;
+        this._canvasElementId = canvasElementId;    // define the canvas HTML element to link with
         this._canvasElement = document.getElementById(this._canvasElementId);
         this._canvasWidth = canvasWidth;
         this._canvasDivisor = 0;
@@ -33,20 +53,33 @@ class CanvasExercise {
         this._orderType = orderType;
     }
 
+    /**
+     * Get constant value for alphabetic ordering objects.
+     * @returns {number}
+     */
     static get orderTypeAlpha() {
         return ORDER_TYPE_ALPHA;
     }
 
+    /**
+     * Get constant value for numeric ordering objects.
+     * @returns {number}
+     */
     static get orderTypeNum() {
         return ORDER_TYPE_NUM;
     }
 
+    /**
+     * Initialize canvas element and load the image.
+     * @private
+     */
     _initializeCanvas() {
 
         if (this._canvasElement.getContext) {
+
+            // getting 2d context from canvas element
             let ctx = this._canvasElement.getContext("2d");
 
-            // Draw images
             let img = new Image(),      // Create new img element
                 that = this;            // referencing this variable
 
@@ -54,18 +87,20 @@ class CanvasExercise {
                 let imageWidth = this.width,
                     imageHeight = this.height;
 
+                // setting canvas divisor to use when width and/or display resolution changes
                 that._canvasDivisor = imageWidth / that._canvasWidth;
 
                 let finalWidth = Math.round(imageWidth / that._canvasDivisor),
                     finalHeight = Math.round(imageHeight / that._canvasDivisor);
 
+                // setting canvas element dimensions
                 that._canvasElement.setAttribute('width', finalWidth.toString());
                 that._canvasElement.setAttribute('height', finalHeight.toString());
 
-                // alert(`cD: ${that._canvasDivisor}, iW: ${imageWidth}, iH: ${imageHeight}, fW: ${finalWidth}, fH: ${finalHeight}`);
+                // drawing image
                 ctx.drawImage(img, 0, 0, imageWidth, imageHeight, 0, 0, finalWidth, finalHeight);
             };
-            // Set source path
+            // Set source path of image
             img.src = this._imageSrc;
         }
     }
--- OrderCanvasExercise.js
+++ OrderCanvasExercise.js
@@ -5,47 +5,82 @@
  * Created by WebStorm
  */
 
-
+/**
+ * OrderCanvasExercise extends from CanvasExercise superclass.
+ * Define the ordering object exercises
+ */
 class OrderCanvasExercise extends CanvasExercise {
 
-    constructor(canvasElementId, canvasWidth = '', imageSrc = '', 
+    /**
+     * Constructor of the OrderCanvasExercise class.
+     * @param canvasElementId
+     * @param canvasWidth
+     * @param imageSrc
+     * @param lineWidth
+     * @param lineColor
+     * @param lineHead
+     * @param fontFamily
+     * @param fontSize
+     * @param fontColor
+     * @param fontStyle
+     * @param fontAlign
+     * @param fontBaseline
+     * @param mark
+     * @param orderType
+     * @param listObjectsCoords
+     * @param strokeRectObject
+     */
+    constructor(canvasElementId, canvasWidth = '', imageSrc = '',
                 lineWidth = '2', lineColor = '#000000', lineHead = false, fontFamily = 'Arial',
                 fontSize = '20', fontColor = '#000000', fontStyle = 'regular', fontAlign = 'center',
                 fontBaseline = 'middle', mark = 'X', orderType = ORDER_TYPE_NUM,
                 // Below are specific class parameters
                 listObjectsCoords = [], strokeRectObject = true,) {
 
-        super(canvasElementId, canvasWidth, imageSrc, cursorStyle, lineWidth, lineColor, lineHead, fontFamily,
-            fontSize, fontColor, mark, orderType);
+        super(canvasElementId, canvasWidth, imageSrc, lineWidth, lineColor, lineHead, fontFamily,
+            fontSize, fontColor, fontStyle, fontAlign, fontBaseline, mark, orderType);
 
-        this._listObjectsCoords = listObjectsCoords;
-        this._listObjectsOrdered = [];
-        this._lastOrderNumber = 0;
-        this._strokeRectObject = strokeRectObject;
+        // own properties of the class
+        this._listObjectsCoords = listObjectsCoords;    // array of objects coordinates in the image
+        this._listObjectsOrdered = [];                  // array to store the array of objects coordinates arranged by user
+        this._lastOrderNumber = 0;                      // last order assigned
+        this._strokeRectObject = strokeRectObject;      // define if object will be put into a rectangle
 
+        // binding click event to canvas element to allow the order exercise execution
         this._canvasElement.addEventListener("click", this.orderObject.bind(this), false);
     }
 
+    /**
+     * Assign an alphabetic/numerical order character to the object selected by click event over canvas element.
+     * This function uses the coordinates of the objects inside canvas element and the coordinates of the click event.
+     * @param event
+     * @returns {*}
+     */
     orderObject(event) {
         // Obtain mouse click position
-        var current_x = event.pageX - event.currentTarget.offsetLeft;
-        var current_y = event.pageY - event.currentTarget.offsetTop;
+        let current_x = event.pageX - event.currentTarget.offsetLeft,
+            current_y = event.pageY - event.currentTarget.offsetTop;
         // alert(`cursorX: ${cur_x}, cursorY: ${cur_y}`);
 
         if (this._canvasElement.getContext) {
-            var ctx = this._canvasElement.getContext("2d");
+            // getting 2d context from canvas element
+            let ctx = this._canvasElement.getContext("2d");
 
+            // getting number of objects by list of object coordinates
             let numberObjects = this._listObjectsCoords.length;
 
+            // asking if object clicked was ordered before
             if (!this._isOrderedExercise(this._listObjectsOrdered, [current_x, current_y], this._canvasDivisor, this._lastOrderNumber, numberObjects)) {
 
                 // declaring variables
-                let x, y, x1, y1, cent_x, cent_y, order, tmp;
+                let x, y, x1, y1, cent_x, cent_y, order, tmp, op;
 
-
-                for (var i = 0; i < this._listObjectsCoords.length; i++) {
+                // looping the list of objects coordinates
+                for (let i = 0; i < this._listObjectsCoords.length; i++) {
 
                     // Coordinates of the images
+                    // The canvas divisor allow to calc the exact position of every coordinate,
+                    // even if the canvas element is positioned anywhere inside the web page
                     x = Math.round(this._listObjectsCoords[i][0] / this._canvasDivisor);
                     y = Math.round(this._listObjectsCoords[i][1] / this._canvasDivisor);
                     x1 = Math.round(this._listObjectsCoords[i][2] / this._canvasDivisor);
@@ -58,41 +93,47 @@ class OrderCanvasExercise extends CanvasExercise {
                     ctx.textBaseline = this._fontBaseline;
 
                     // alert(x + " <= " + cur_x + " - " + cur_x + " <= " + x1 + " --- " + y + " <= " + cur_y + " - " + cur_y + " <= " + y1);
-                    var op = false;
+
+                    // loop control variable
+                    op = false;
+
+                    // asking if a valid object was clicked, based on coordinates of objects and click event
                     if ((x <= current_x && current_x <= x1)
                         && (y <= current_y && current_y <= y1)
                         && (this._lastOrderNumber < numberObjects)) {
-                        //alert(this._lastOrderNumber);
+
+                        // increment order number to assign
                         this._lastOrderNumber++;
 
-                        // coordinates of the image center
+                        // coordinates of the object center
                         cent_x = Math.round(x1 - ((x1 - x) / 2));
                         cent_y = Math.round(y1 - ((y1 - y) / 2));
 
                         // write order number
-
                         order = this._lastOrderNumber.toString();
+
+                        // asking what kind of order is being used
                         if (this._orderType === CanvasExercise.orderTypeAlpha) {
+
+                            // if alphabetic order was selected, then calculate the letter order
                             tmp = Math.floor(this._lastOrderNumber / 26);
                             order = (tmp >= 1 ? String.fromCharCode(64 + (this._lastOrderNumber / 26)) : '')
                                 + String.fromCharCode(64 + (this._lastOrderNumber % 26));
                         }
+
+                        // fill number or letter of the order
                         ctx.fillText(order, cent_x, cent_y);
 
-                        // Fill rectangle
+                        // fill rectangle
                         if (this._strokeRectObject) {
                             ctx.lineWidth = this._lineWidth;
                             ctx.strokeStyle = this._lineColor;
                             ctx.strokeRect(x, y, x1 - x, y1 - y);
                         }
 
-                        // var lastAnswer = $("#answers_order").html();
-                        // $("#answers_order").html(lastAnswer + "[" + this._listObjectsCoords[i][0] + "," + this._listObjectsCoords[i][1] + "," + this._listObjectsCoords[i][2] + "," + this._listObjectsCoords[i][3] + "]");
-
-                        // update ordered exercises matrix
+                        // update list of ordered objects
                         this._listObjectsOrdered[this._listObjectsOrdered.length] = this._listObjectsCoords[i];
 
-                        //alert(x + " < " + cent_x + " < " + x1);
                         op = true;
                     }
 
@@ -105,10 +146,19 @@ class OrderCanvasExercise extends CanvasExercise {
         }
     }
 
-
+    /**
+     * Determine if the element selected by click coordinates have been ordered before.
+     * @param objectsOrdered
+     * @param current_xy
+     * @param divisor
+     * @param lastOrderNumber
+     * @param numberObjects
+     * @returns {boolean} Return true if the element selected by click coordinates have been ordered before.
+     * @private
+     */
     _isOrderedExercise(objectsOrdered, current_xy, divisor, lastOrderNumber, numberObjects) {
 
-        for (var i = 0; i < objectsOrdered.length; i++) {
+        for (let i = 0; i < objectsOrdered.length; i++) {
 
             // Coordinates of the images
             let x = Math.round(objectsOrdered[i][0] / divisor),
@@ -126,6 +176,10 @@ class OrderCanvasExercise extends CanvasExercise {
         return false;
     }
 
+    /**
+     * Return and print in JS console the array of the list of ordered objects
+     * @returns {Array}
+     */
     getListObjectsOrdered() {
         console.log(JSON.stringify(this._listObjectsOrdered));
         return this._listObjectsOrdered;
@@ -136,13 +190,15 @@ class OrderCanvasExercise extends CanvasExercise {
 OrderCanvasExercise.exports = OrderCanvasExercise;
 
 
+
+// Live execution of code
 let coords = [[80, 35, 375, 283], [395, 120, 715, 370], [36, 352, 88, 404], [520, 20, 590, 76]];
 
 const ex = new OrderCanvasExercise('myCanvas');
 ex._listObjectsCoords = coords;
 ex._canvasWidth = 700;
 ex._imageSrc = 'plasma-desktop.jpg';
-// ex._orderType = CanvasExercise.orderTypeAlpha;
+ex._orderType = CanvasExercise.orderTypeAlpha;
 ex._initializeCanvas();
 
 ex._canvasElement.addEventListener("click", function () {
