1191a3ca770fcc2c1a992ecc4cd8d802e9223dcf
yulioaj290 <yulioaj290@github.com>
2018-09-30 11:39:18 -0400

    [UPDATE] Relocated JavaScript compiled files by Babel following isomorphic workflow.

--- build/js/GapMatchCanvasExercise-compiled.js
+++ build/js/GapMatchCanvasExercise-compiled.js
@@ -9,21 +9,21 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 /**
- * Project: wice.js.
+ * Project: ICE.js.
  * Author: Yulio Aleman Jimenez [@yulioaj290]
- * Date: 4/09/18
+ * Date: 8/28/18
  * Created by WebStorm
  */
 
 /**
- * PositionObjectCanvasExercise extends from CanvasExercise superclass.
- * Define the position object exercises
+ * GapMatchCanvasExercise extends from CanvasExercise superclass.
+ * Define the gap matching exercises
  */
-var PositionObjectCanvasExercise = function (_CanvasExercise) {
-    _inherits(PositionObjectCanvasExercise, _CanvasExercise);
+var GapMatchCanvasExercise = function (_CanvasExercise) {
+    _inherits(GapMatchCanvasExercise, _CanvasExercise);
 
     /**
-     * Constructor of the PositionObjectCanvasExercise class.
+     * Constructor of the GapMatchCanvasExercise class.
      * @param canvasElementId
      * @param canvasWidth
      * @param imageSrc
@@ -39,9 +39,10 @@ var PositionObjectCanvasExercise = function (_CanvasExercise) {
      * @param mark
      * @param orderType
      * @param listObjectsCoords
+     * @param listObjectsTags
      * @param strokeRectObject
      */
-    function PositionObjectCanvasExercise(canvasElementId) {
+    function GapMatchCanvasExercise(canvasElementId) {
         var canvasWidth = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
         var imageSrc = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
         var lineWidth = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '2';
@@ -56,35 +57,35 @@ var PositionObjectCanvasExercise = function (_CanvasExercise) {
         var mark = arguments.length > 12 && arguments[12] !== undefined ? arguments[12] : 'X';
         var orderType = arguments.length > 13 && arguments[13] !== undefined ? arguments[13] : ORDER_TYPE_NUM;
         var listObjectsCoords = arguments.length > 14 && arguments[14] !== undefined ? arguments[14] : [];
-        var strokeRectObject = arguments.length > 15 && arguments[15] !== undefined ? arguments[15] : true;
+        var listObjectsTags = arguments.length > 15 && arguments[15] !== undefined ? arguments[15] : [];
+        var strokeRectObject = arguments.length > 16 && arguments[16] !== undefined ? arguments[16] : true;
 
-        _classCallCheck(this, PositionObjectCanvasExercise);
+        _classCallCheck(this, GapMatchCanvasExercise);
 
         // own properties of the class
-        var _this = _possibleConstructorReturn(this, (PositionObjectCanvasExercise.__proto__ || Object.getPrototypeOf(PositionObjectCanvasExercise)).call(this, canvasElementId, canvasWidth, imageSrc, lineWidth, lineColor, lineHead, fontFamily, fontSize, fontColor, fontStyle, fontAlign, fontBaseline, mark, orderType));
+        var _this = _possibleConstructorReturn(this, (GapMatchCanvasExercise.__proto__ || Object.getPrototypeOf(GapMatchCanvasExercise)).call(this, canvasElementId, canvasWidth, imageSrc, lineWidth, lineColor, lineHead, fontFamily, fontSize, fontColor, fontStyle, fontAlign, fontBaseline, mark, orderType));
 
         _this._listObjectsCoords = listObjectsCoords; // array of objects coordinates in the image
-        _this._listObjectsAssociated = []; // array to store the array of objects coordinates positioned by user
+        _this._listObjectsTags = listObjectsTags; // array of objects tags in the image
+        _this._listObjectsTagged = []; // array to store the array of objects coordinates tagged by user
         _this._strokeRectObject = strokeRectObject; // define if object will be put into a rectangle
-        _this._numberAssociationsConnected = 0;
 
-        // binding click event to canvas element to allow the position object exercise execution
+        // binding click event to canvas element to allow the gap match exercise execution
         _this._canvasElement.addEventListener("click", _this.clickAction.bind(_this), false);
         return _this;
     }
 
     /**
-     * Assign a position based on click event over canvas element.
+     * Assign a Tag of object selected by click event over canvas element.
      * This function uses the coordinates of the objects inside canvas element and the coordinates of the click event.
      * @param event
      * @returns {*}
      */
 
 
-    _createClass(PositionObjectCanvasExercise, [{
+    _createClass(GapMatchCanvasExercise, [{
         key: 'clickAction',
         value: function clickAction(event) {
-
             // Obtain mouse click position
             var current_x = event.pageX - event.currentTarget.offsetLeft,
                 current_y = event.pageY - event.currentTarget.offsetTop;
@@ -94,27 +95,17 @@ var PositionObjectCanvasExercise = function (_CanvasExercise) {
                 // getting 2d context from canvas element
                 var ctx = this._canvasElement.getContext("2d");
 
-                // asking if there is elements to be positioned
-                if (this._numberAssociationsConnected < this._listObjectsCoords.length) {
+                // asking if object clicked was matched before
+                if (!this._isTaggedExercise(this._listObjectsTagged, [current_x, current_y], this._canvasDivisor, this._listObjectsTags.length)) {
 
                     // declaring variables
                     var x = void 0,
                         y = void 0,
                         x1 = void 0,
                         y1 = void 0,
+                        cent_x = void 0,
+                        cent_y = void 0,
                         op = void 0;
-
-                    // init text properties
-                    ctx.font = this._fontStyle + ' ' + this._fontSize + '  ' + this._fontFamily;
-                    ctx.fillStyle = this._fontColor;
-                    ctx.textAlign = this._fontAlign;
-                    ctx.textBaseline = this._fontBaseline;
-
-                    // fill mark or letter
-                    ctx.fillText(this._mark, current_x, current_y);
-
-                    // decrement number of objects to be positioned
-                    this._numberAssociationsConnected++;
 
                     // looping the list of objects coordinates
                     for (var i = 0; i < this._listObjectsCoords.length; i++) {
@@ -127,23 +118,34 @@ var PositionObjectCanvasExercise = function (_CanvasExercise) {
                         x1 = Math.round(this._listObjectsCoords[i][2] / this._canvasDivisor);
                         y1 = Math.round(this._listObjectsCoords[i][3] / this._canvasDivisor);
 
-                        // alert(x + " <= " + cur_x + " - " + cur_x + " <= " + x1 + " --- " + y + " <= " + cur_y + " - " + cur_y + " <= " + y1);
+                        // init text properties
+                        ctx.font = this._fontStyle + ' ' + this._fontSize + '  ' + this._fontFamily;
+                        ctx.fillStyle = this._fontColor;
+                        ctx.textAlign = this._fontAlign;
+                        ctx.textBaseline = this._fontBaseline;
 
                         // loop control variable
                         op = false;
 
                         // asking if a valid object was clicked, based on coordinates of objects and click event
-                        if (x <= current_x && current_x <= x1 && y <= current_y && current_y <= y1) {
+                        if (x <= current_x && current_x <= x1 && y <= current_y && current_y <= y1 && this._listObjectsTags.length > 0) {
 
-                            // fill rectangle
+                            // coordinates of the object center
+                            cent_x = Math.round(x1 - (x1 - x) / 2);
+                            cent_y = Math.round(y1 - (y1 - y) / 2);
+
+                            // write tag string
+                            ctx.fillText(this._listObjectsTags[0], cent_x, cent_y);
+
+                            // Fill rectangle
                             if (this._strokeRectObject) {
                                 ctx.lineWidth = this._lineWidth;
                                 ctx.strokeStyle = this._lineColor;
                                 ctx.strokeRect(x, y, x1 - x, y1 - y);
                             }
 
-                            // update list of positioned objects
-                            this._listObjectsAssociated[this._listObjectsAssociated.length] = this._listObjectsCoords[i];
+                            // update tagged exercises
+                            this._listObjectsTagged[this._listObjectsTagged.length] = [this._listObjectsTags.shift(), this._listObjectsCoords[i]];
 
                             op = true;
                         }
@@ -152,50 +154,76 @@ var PositionObjectCanvasExercise = function (_CanvasExercise) {
                             break;
                         }
                     }
-                    return this.getListObjectsPositioned();
                 }
             }
         }
 
         /**
-         * Return and print in JS console the array of the list of positioned objects
+         * Return an array of objects coordinates in the image
+         * @returns {Array|*}
+         */
+
+    }, {
+        key: 'getListObjectsCoords',
+        value: function getListObjectsCoords() {
+            console.log(JSON.stringify(this._listObjectsCoords));
+            return this._listObjectsCoords;
+        }
+
+        /**
+         * Return an array of objects tags in the image
+         * @returns {Array|*}
+         */
+
+    }, {
+        key: 'getListObjectsTags',
+        value: function getListObjectsTags() {
+            console.log(JSON.stringify(this._listObjectsTags));
+            return this._listObjectsTags;
+        }
+
+        /**
+         * Return an array of arrays. Each one contains the object coordinate and the tag assigned
          * @returns {Array}
          */
 
     }, {
-        key: 'getListObjectsPositioned',
-        value: function getListObjectsPositioned() {
-            console.log(JSON.stringify(this._listObjectsAssociated));
-            return this._listObjectsAssociated;
+        key: 'getListObjectsTagged',
+        value: function getListObjectsTagged() {
+            console.log(JSON.stringify(this._listObjectsTagged));
+            return this._listObjectsTagged;
+        }
+
+        /**
+         * Determine if the element selected by click coordinates have been matched before.
+         * @param objectsTagged
+         * @param current_xy
+         * @param divisor
+         * @param cantTagged
+         * @returns {boolean}
+         * @private
+         */
+
+    }, {
+        key: '_isTaggedExercise',
+        value: function _isTaggedExercise(objectsTagged, current_xy, divisor, cantTagged) {
+            for (var i = 0; i < objectsTagged.length; i++) {
+
+                // Coordinates of the images
+                var x = Math.round(objectsTagged[i][0] / divisor),
+                    y = Math.round(objectsTagged[i][1] / divisor),
+                    x1 = Math.round(objectsTagged[i][2] / divisor),
+                    y1 = Math.round(objectsTagged[i][3] / divisor);
+
+                if (x <= current_xy[0] && current_xy[0] <= x1 && y <= current_xy[1] && current_xy[1] <= y1 && cantTagged > 0) {
+                    return true;
+                }
+            }
+            return false;
         }
     }]);
 
-    return PositionObjectCanvasExercise;
+    return GapMatchCanvasExercise;
 }(CanvasExercise);
 
-PositionObjectCanvasExercise.exports = PositionObjectCanvasExercise;
-
-// // Live execution of code
-// let coords = [[80, 35, 375, 283], [395, 120, 715, 370], [36, 352, 88, 404], [520, 20, 590, 76]];
-//
-// const ex = new PositionObjectCanvasExercise('myCanvas');
-// ex._listObjectsCoords = coords;
-// ex._canvasWidth = 700;
-// ex._imageSrc = 'plasma-desktop.jpg';
-// ex.initializeCanvas();
-//
-// let marks = [];
-//
-// for (let i = 0; i < coords.length; i++) {
-//     marks[i] = ex.getMark();
-// }
-//
-// document.getElementById("options").innerText = JSON.stringify(marks);
-//
-// ex._canvasElement.addEventListener("click", function () {
-//     marks.shift();
-//     document.getElementById("options").innerText = JSON.stringify(marks);
-//     document.getElementById("result").innerText = JSON.stringify(ex.getListObjectsAssociated());
-// }, false);
-//
-// console.log(ex);
+GapMatchCanvasExercise.exports = GapMatchCanvasExercise;
